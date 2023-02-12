9e3d2e6433053c6dc4965a22d12950e24547de87
Leon Timmermans <fawaka@gmail.com>
2023-02-12 17:32:19 +0100

    Update the Crypt::Passphrase::Reversed in t/lib

--- t/lib/Crypt/Passphrase/Reversed.pm
+++ t/lib/Crypt/Passphrase/Reversed.pm
@@ -14,20 +14,22 @@ sub new {
 
 sub hash_password {
 	my ($self, $password) = @_;
-	return scalar reverse decode_utf8($password);
+	return '$reversed$' . reverse decode_utf8($password);
 }
 
 sub needs_rehash {
-	return 1;
+	my ($self, $hash) = @_;
+	return substr($hash, 0, 10) ne '$reversed$';
 }
 
-sub accepts_hash {
-	return 1;
+sub crypt_subtypes {
+	return 'reversed';
 }
 
 sub verify_password {
 	my ($class, $password, $hash) = @_;
-	return scalar(reverse decode_utf8($password)) eq $hash;
+	return 0 unless substr($hash, 0, 10) eq '$reversed$';
+	return scalar(reverse decode_utf8($password)) eq substr $hash, 10;
 }
 
 1;
